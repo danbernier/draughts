7bd13655d84a99e964b62065626302a8a5998450
Dan Bernier <danbernier@gmail.com>
2016-12-08 15:54:03 -0500

    Make a DrawRect from Signals for width & height

--- src/draughts/DrawRect.java
+++ src/draughts/DrawRect.java
@@ -1,15 +1,19 @@
 package draughts;
 
+import signals.*;
 import processing.core.PApplet;
 
 public class DrawRect implements Draught {
-  private float w;
-  private float h;
+  private Signal<Float> w;
+  private Signal<Float> h;
   public DrawRect(float w, float h) {
+    this(new Constant<Float>(w), new Constant<Float>(h));
+  }
+  public DrawRect(Signal<Float> w, Signal<Float> h) {
     this.w = w;
     this.h = h;
   }
   public void draw(PApplet sketch) {
-    sketch.rect(0, 0, w, h);
+    sketch.rect(0, 0, w.current(), h.current());
   }
 }
