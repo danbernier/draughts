f01590c2509f04966a170446b65a5cf0c90f2127
Dan Bernier <danbernier@gmail.com>
2016-12-23 16:25:08 -0500

    Construct a Rotate with a Signal<Float>

--- src/draughts/Rotate.java
+++ src/draughts/Rotate.java
@@ -1,13 +1,17 @@
 package draughts;
 
 import processing.core.PApplet;
+import signals.*;
 
 public class Rotate implements Draught {
-  float theta;
+  private Signal<Float> theta;
   public Rotate(float theta) {
+    this(new Constant<Float>(theta));
+  }
+  public Rotate(Signal<Float> theta) {
     this.theta = theta;
   }
   public void draw(PApplet sketch) {
-    sketch.rotate(theta);
+    sketch.rotate(theta.current());
   }
 }
