3da6374cd27d15cf2201dcde599e91a3b2324312
Dan Bernier <danbernier@gmail.com>
2018-02-19 12:05:28 -0500

    Add x,y params to DrawRect

--- src/draughts/DrawRect.java
+++ src/draughts/DrawRect.java
@@ -8,16 +8,31 @@ import processing.core.PApplet;
  */
 
 public class DrawRect implements Draught {
+  private Signal<Float> x;
+  private Signal<Float> y;
   private Signal<Float> width;
   private Signal<Float> height;
+
   public DrawRect(float width, float height) {
-    this(new Constant<Float>(width), new Constant<Float>(height));
+    this(new Constant<Float>(0.0f), new Constant<Float>(0.0f), new Constant<Float>(width), new Constant<Float>(height));
   }
   public DrawRect(Signal<Float> width, Signal<Float> height) {
+    this(new Constant<Float>(0.0f), new Constant<Float>(0.0f), width, height);
+  }
+  public DrawRect(float x, float y, float width, float height) {
+    this(new Constant<Float>(x), new Constant<Float>(y), new Constant<Float>(width), new Constant<Float>(height));
+  }
+  public DrawRect(float x, float y, Signal<Float> width, Signal<Float> height) {
+    this(new Constant<Float>(x), new Constant<Float>(y), width, height);
+  }
+  public DrawRect(Signal<Float> x, Signal<Float> y, Signal<Float> width, Signal<Float> height) {
+    this.x = x;
+    this.y = y;
     this.width = width;
     this.height = height;
   }
+
   public void draw(PApplet sketch) {
-    sketch.rect(0, 0, width.current(), height.current());
+    sketch.rect(x.current(), y.current(), width.current(), height.current());
   }
 }
