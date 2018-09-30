57a14aa57e9f8fdb71d72b40c39849a98caf807e
Dan Bernier <danbernier@gmail.com>
2018-09-29 22:02:55 -0500

    document how Osc's fluent interface works

--- src/signals/Osc.java
+++ src/signals/Osc.java
@@ -7,6 +7,18 @@ package signals;
  * interface](https://stackoverflow.com/questions/17937755/what-is-the-difference-between-a-fluent-interface-and-the-builder-pattern)
  * for specifying amplitude, phase, and offset (basically, translating the
  * signal up or down - it's the last thing added to the signal value).
+ *
+ * Examples:
+ *
+ * ```
+ * Signal osc = new Osc(1).amp(200).phase(3).offset(11);
+ * ```
+ *
+ * ...though tbh if you want to use `amp` or `offset`, I'd suggest using this, to rely on fewer weird parts, and more standard parts:
+ *
+ * ```
+ * Signal osc = new Shift(11, new Amp(200, new Osc(1).phase(3)));
+ * ```
  */
 
 public class Osc implements Signal<Float> {
