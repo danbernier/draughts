e3476bfff53b8afbe803a9a85dffb377cae2fe2d
Dan Bernier <danbernier@gmail.com>
2018-09-29 22:02:41 -0500

    fml. why does pvectorsignal disappear? fuck it. hack it.

--- src/draughts/Translate.java
+++ src/draughts/Translate.java
@@ -10,19 +10,29 @@ import processing.core.PVector;
  */
 
 public class Translate implements Draught {
-  Signal<PVector> position;
+  // FML, why isn't PVectorSignal coming through?
+  // TODO find & fix this.
+  //Signal<PVector> position;
+  Signal<Float> x;
+  Signal<Float> y;
+
   public Translate(float x, float y) {
-    this(new Constant<PVector>(new PVector(x, y)));
+    //this(new Constant<PVector>(new PVector(x, y)));
+    this(new Constant<Float>(x), new Constant<Float>(y));
   }
+  //public Translate(Signal<Float> x, Signal<Float> y) {
+  //  this(new PVectorSignal(x, y));
+  //}
+  //public Translate(Signal<PVector> position) {
+  //  this.position = position;
+  //}
   public Translate(Signal<Float> x, Signal<Float> y) {
-    this(new PVectorSignal(x, y));
-  }
-  public Translate(Signal<PVector> position) {
-    this.position = position;
+    this.x = x;
+    this.y = y;
   }
   public void draw(PApplet sketch) {
-    PVector pos = position.current();
-    sketch.translate(pos.x, pos.y);
+    //PVector pos = position.current();
+    sketch.translate(x.current(), y.current());
   }
 }
 
